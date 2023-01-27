56d74bcbe396d4f0e33fc0f0db69075fe2d965a7
Anthony <86444119+anthms@users.noreply.github.com>
2023-01-27 13:56:32 -0600

    Update api-call.sh

--- api-call.sh
+++ api-call.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sw '%{http_code}' \
+curl -s --fail-with-body \
 -H "Accept: application/vnd.github+json" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
 https://api.github.com/repos/$REPO
